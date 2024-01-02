5539693d386dea3f4afd3225e001f641e00f3543
ChoiHs <chs991209@gmail.com>
2024-01-02 10:32:58 +0900

    ADD: Initial Setting에 TypeOrm Config, Validation Pipe, Cors 포함하도록 작성

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AnimalsModule } from './animals/animals.module';
+import * as process from 'process';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [`${__dirname}/**/entities/*.entity.{ts,js}`],
+      synchronize: false,
+      logging: true,
+    }),
+    AnimalsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
